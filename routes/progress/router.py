ef38a81fcea233f2e9b96476366950efc778e340
Treew666 <daniilca2012@mail.ru>
2026-07-12 13:55:37 +0300

    fix: Исправил код статуса ошибки (исправил 300 - несколько вариантов на 400 - неправильный запрос) refactor: Переместил модели (Answer, ProgressRequest) в отдельный файл types.py

--- routes/progress/router.py
+++ routes/progress/router.py
@@ -1,19 +1,29 @@
-from typing import Any
 from fastapi import APIRouter
-from ..types import answer
-from functions.processing_json import is_user_id_in_vault
+from ..types import Answer, ProgressRequest
+from functions.processing_json import is_user_id_in_vault, add_progress_user
 
 
 router = APIRouter()
 
 
-from pydantic import BaseModel
-
-class answer(BaseModel):
-    status_code: int
-    info: str
-
 @router.post('/{user_id}')
-async def add_progress(user_id: str, progress_name: str) -> answer:
-    if is_user_id_in_vault(user_id=user_id):
-        pass
+async def add_progress(user_id: str, body: ProgressRequest) -> Answer:
+    '''Добавление прогресса'''
+    progress_name = body.progress_name
+    try:
+        if is_user_id_in_vault(user_id=user_id):
+            text_info = f'successful add progress, {progress_name}, for user, {user_id}'
+        else:
+            text_info = f'successful add user, {user_id}, and progress, {progress_name}'
+            
+        add_progress_user(user_id=user_id, progress_name=progress_name)
+        return Answer(
+            status_code = 200,
+            info = text_info
+        )
+        
+    except ValueError:
+        return Answer(
+            status_code = 400,
+            info = f'the user, {user_id}, already has progress, {progress_name}'
+        )
--- routes/types.py
+++ routes/types.py
@@ -1,2 +1,10 @@
+from pydantic import BaseModel
 
+
+class Answer(BaseModel):
+    status_code: int
+    info: str
     
+
+class ProgressRequest(BaseModel):
+    progress_name: str
