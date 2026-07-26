640c09a2b9afa5e5f80ff8269f88d7b737dadd9d
Treew666 <daniilca2012@mail.ru>
2026-07-26 15:41:55 +0300

    fix: изменил название колонок параметров теста, изменил название параметров функции теста, добавил типы параметров

--- tests/test_progress.py
+++ tests/test_progress.py
@@ -5,7 +5,7 @@ from routes.types import ProgressRequest
 
 
 @pytest.mark.parametrize(
-    'user_id, progress, expected',
+    'user_id, progress_name, status_code',
     [
         ('1100', 'progress1', 200),
         ('1101', 'progress1', 400),
@@ -13,7 +13,7 @@ from routes.types import ProgressRequest
         ('1102', 'progress1', 400),
     ],
 )
-async def test_add_progress(user_id, progress, expected) -> None:
+async def test_add_progress(user_id: str, progress_name: str, status_code: int) -> None:
     '''Тест на добавление прогресса'''
-    result = await add_progress(user_id=user_id, body=ProgressRequest(progress_name=progress))
-    assert result.status_code == expected
+    result = await add_progress(user_id=user_id, body=ProgressRequest(progress_name=progress_name))
+    assert result.status_code == status_code
