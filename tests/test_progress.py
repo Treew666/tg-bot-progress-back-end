827bf7934c5f2c749c3b92638b303bc35f8e66d9
Treew666 <daniilca2012@mail.ru>
2026-07-28 18:30:57 +0300

    test: написал тест для ручки DELETE /progress/user_id/Progress_name

--- tests/test_progress.py
+++ tests/test_progress.py
@@ -1,11 +1,11 @@
 import pytest
 
-from routes.progress.router import add_progress
+from routes.progress.router import add_progress, delete_progress
 from routes.types import ProgressRequest
 
 
 @pytest.mark.parametrize(
-    "user_id, progress, expected",
+    "user_id, progress_name, status_code",
     [
         ("1100", "progress1", 200),
         ("1101", "progress1", 400),
@@ -13,7 +13,22 @@ from routes.types import ProgressRequest
         ("1102", "progress1", 400),
     ],
 )
-async def test_add_progress(user_id, progress, expected) -> None:
+async def test_add_progress(user_id: str, progress_name: str, status_code: int) -> None:
     """Тест на добавление прогресса"""
-    result = await add_progress(user_id=user_id, body=ProgressRequest(progress_name=progress))
-    assert result.status_code == expected
+    result = await add_progress(user_id=user_id, body=ProgressRequest(progress_name=progress_name))
+    assert result.status_code == status_code
+
+
+@pytest.mark.parametrize(
+    "user_id, progress_name, status_code",
+    [
+        ("1101", "progress1", 200),
+        ("1101", "progress4", 400),
+        ("1109", "progress3", 400),
+        ("1102", "progress1", 200),
+    ],
+)
+async def test_delete_progress(user_id: str, progress_name: str, status_code: int) -> None:
+    """Тест на удаление прогресса"""
+    result = await delete_progress(user_id=user_id, progress_name=progress_name)
+    assert result.status_code == status_code
