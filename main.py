b724f14125606dc91da47136924706a92905bb9c
Daniil Gaydin <133574424+Treew666@users.noreply.github.com>
2026-08-05 18:55:19 +0300

    hotfix: изменил main на правильный формат

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
+from routes.progress.ready.router import router as progress_ready_router
 from routes.progress.router import router as progress_router
 
 load_dotenv()
 
 app = FastAPI()
 
-
-def main() -> None:
-    app.include_router(progress_router, prefix="/progress")
-
-
-if __name__ == "__main__":
-    main()
+app.include_router(progress_router, prefix="/progress")
+app.include_router(progress_ready_router, prefix="/progress/ready")
