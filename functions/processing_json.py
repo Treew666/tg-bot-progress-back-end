9f1e4a966ef423535d82671eee73f006674b2238
Daniil Gaydin <133574424+Treew666@users.noreply.github.com>
2026-08-05 18:25:26 +0300

    feature/post-progress-ready-user_id
    * feat: создал функцию отмечающию прогресс сегодня выполненым, то есть добавляет дату в список прогресса пользователя
    
    * docs: убрана одна лишняя табуляция
    
    * test: добавил в тестовую БД добавление сегодняшней даты для пользователя 1102 и прогресса progress2. Это нужно для проверки на отработку ошибки при добавлении сегодняшней даты.
    
    * test: создал тест для ручки ready_progress
    
    * refactor: добавил возвращаемые данные (сегодняшнию дату) для функцию add_ready_user_progress
    
    * docs: написал разделы Args, Returns для функции add_ready_user_progress
    
    * feat: написал ручку POST progress/ready/user_id
    
    * refactor: ruff написал подключение библиотек под стандарты
    
    * refactor: ruff format отформатировал

--- functions/processing_json.py
+++ functions/processing_json.py
@@ -1,5 +1,7 @@
 import json
 import os
+from datetime import date
+from typing import NewType
 
 
 def read_vault_json() -> dict:
@@ -91,3 +93,38 @@ def delete_progress_user(user_id: str, progress_name: str) -> None:
         write_vault_json(vault=vault)  # raise ValueError при пустой ссылки на БД
     else:
         raise ValueError(f"the progress, {progress_name}, not found for the user, {user_id}")
+
+
+DateString = NewType("DateString", str)
+
+
+def add_ready_user_progress(user_id: str, progress_name: str) -> DateString:
+    """Добавляем отметку (сегодняшнию дату) в список прогресса
+
+    Args:
+        user_id: id пользователя в БД.
+        progress_name: название прогресса в БД.
+
+    Returns:
+        DateString: сегодняшняя дата в формате DD.MM.YYYY, которая была добавлена в список прогресса.
+
+    Raises:
+        ValueError: если url БД не существует.
+        ValueError: пользователь не найден.
+        ValueError: сегодяншяя дату уже добалена.
+    """
+    vault = read_vault_json()  # raise ValueError при пустой ссылки на БД
+    if user_id not in vault:
+        raise ValueError(f"user id, {user_id}, not found")
+    if progress_name not in vault[user_id]:
+        raise ValueError(f"the progress, {progress_name}, not found for the user, {user_id}")
+
+    today = date.today().strftime("%d.%m.%Y")
+    if today in vault[user_id][progress_name]:
+        raise ValueError(
+            f"Today's progress, {progress_name}, is already marked for user, {user_id}"
+        )
+
+    vault[user_id][progress_name].append(today)
+    write_vault_json(vault=vault)
+    return DateString(today)
