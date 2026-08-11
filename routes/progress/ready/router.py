2ab3ee840d93bbce14ae7501d66964709aebe969
Daniil Gaydin <133574424+Treew666@users.noreply.github.com>
2026-08-11 17:29:29 +0300

    Feature/delete progress ready user id progresss name (#18)
    * refactor: изменил проверку на использование уже существующийх функций в функции add_ready_user_progress
    
    * docs: добавил пропущенную ошибку в функции add_ready_user_progress
    
    * feat: написал функцию delete_ready_user_progress для удаления отметки
    
    * feat: написал ручку /progress/ready/user_id/progress_name
    
    * refactor: отформатировал файл
    
    * style: коректно раставил импортов
    
    * feat: написал тест для ручки DELETE /progress/ready/user_id/progress_name
    
    * fix: почему-то не удаленные файлы, которым поменяли расширение

--- routes/progress/ready/router.py
+++ routes/progress/ready/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from functions.processing_json import (
     add_ready_user_progress,
+    delete_ready_user_progress,
     is_progress_in_user_id,
     is_user_id_in_vault,
 )
@@ -25,6 +26,25 @@ async def ready_progress(user_id: str, body: ProgressRequest) -> Answer:
         return Answer(
             status_code=200,
             info=f"successful add today date, {today}, in progress, {progress_name}, for user, {user_id}",
+        )
+    except ValueError as valueError:
+        return Answer(status_code=400, info=str(valueError))
+
+
+@router.delete("/{user_id}/{progress_name}")
+async def delete_ready_progress(user_id: str, progress_name: str) -> Answer:
+    """Удаления отметки (даты) выполнения прогресса"""
+    if not is_user_id_in_vault(user_id=user_id):
+        return Answer(status_code=400, info=f"the user, {user_id}, not found")
+    if not is_progress_in_user_id(user_id=user_id, progress_name=progress_name):
+        return Answer(
+            status_code=400, info=f"the user, {user_id}, has no progress, {progress_name}"
+        )
+    try:
+        today = delete_ready_user_progress(user_id=user_id, progress_name=progress_name)
+        return Answer(
+            status_code=200,
+            info=f"successful delete today date, {today}, in progress, {progress_name}, for user, {user_id}",
         )
     except ValueError as valueError:
         return Answer(status_code=400, info=str(valueError))
--- tests/test_progress_ready.py
+++ tests/test_progress_ready.py
@@ -1,6 +1,6 @@
 import pytest
 
-from routes.progress.ready.router import ready_progress
+from routes.progress.ready.router import delete_ready_progress, ready_progress
 from routes.types import ProgressRequest
 
 
@@ -18,4 +18,19 @@ async def test_ready_progress(user_id: str, progress_name: str, status_code: int
     result = await ready_progress(
         user_id=user_id, body=ProgressRequest(progress_name=progress_name)
     )
+    assert result.status_code == status_code
+
+
+@pytest.mark.parametrize(
+    "user_id, progress_name, status_code",
+    [
+        ("1101", "progress1", 400),
+        ("1101", "progress2", 200),
+        ("1102", "progress1", 400),
+        ("1102", "progress2", 400),
+    ],
+)
+async def test_delete_ready_progress(user_id: str, progress_name: str, status_code: int) -> None:
+    """Тест на отметку сегодняшнего дня"""
+    result = await delete_ready_progress(user_id=user_id, progress_name=progress_name)
     assert result.status_code == status_code
